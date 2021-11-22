7b0abf3188a9d86faa3cea57f03f43e2b5612135
Cosmat <cosmat@mail.ru>
2021-11-22 15:35:58 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,55 +1,43 @@
-const https = require("https");
-const consola = require("consola");
-const app = require("./app");
-const { URL } = require("url");
+
+var fs = require("fs")
+const http = require("http")
+const https = require("https")
+var sslcert = fs.readFileSync(
+  "/etc/letsencrypt/live/rabota-nt.ru/fullchain.pem",
+  "utf8"
+)
+var sslkey = fs.readFileSync(
+  "/etc/letsencrypt/live/rabota-nt.ru/privkey.pem",
+  "utf8"
+)
+var dhparam = fs.readFileSync("/home/pi/Desktop/v_07/server/dhparam.pem", "utf8")
+var options = {
+  key: sslkey,
+  cert: sslcert,
+  dhparam: dhparam,
+}
+
+const app = require("./app")
 
 async function start() {
-  const host = "localhost";
-  const port = 5000;
+  console.log(`Starting Server!`)
+  // var httpServer = http.createServer(app)
+  var httpsServer = https.createServer(options, app)
 
-  // Give nuxt middleware to express
-
-  // const port = 1000
-
-  // Listen the server
-
-  var fs = require("fs");
-  // var sslcert = fs.readFileSync(
-  //   "/etc/letsencrypt/live/rabota-nt.ru/cert.pem",
-  //   "utf8"
-  // );
-  // var sslkey = fs.readFileSync(
-  //   "/etc/letsencrypt/live/rabota-nt.ru/privkey.pem",
-  //   "utf8"
-  // );
-  // var options = {
-  //   key: sslkey,
-  //   cert: sslcert,
-  //   passphrase: "babylon5",
-  // };
-
-  // const server = https.createServer(options, app);
-  // console.log("Server trying starting");
-  // server.listen(port, "0.0.0.0", () => {
-  //   consola.ready({
-  //     message: `Server listening on https://${host}:${port}`,
-  //     badge: true,
-  //   });
-  // });
-
-  const http = require("http");
-  console.log(`Starting Server listening on http://${host}:${port}`);
+  const http = require("http")
+  
   http
     .createServer((req, res) => {
       res.writeHead(301, {
-        // Location: "https://" + req.headers["host"] + req.url,
         Location: "https://rabota-nt.ru",
-      });
-      //res.send("Hello World!");
+      })
 
-      res.end();
+      res.end()
     })
-    .listen(80);
+    .listen(8080)
+
+  // httpServer.listen(8080)
+  httpsServer.listen(8443)
 }
 
-start();
+start()
